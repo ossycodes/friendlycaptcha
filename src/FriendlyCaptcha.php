9c06043168e357d08167622c0891ceb3f1d4669b
ossycodes <ossyfizy009@gmail.com>
2022-10-21 15:12:32 +0100

    set up friendlyCaptcha class wip

---
+++ src/FriendlyCaptcha.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ossycodes\FriendlyCaptcha;
+
+class FriendlyCaptcha
+{
+    const VERIFY_API =  'https://api.friendlycaptcha.com/api/v1/siteverify';
+
+    /**
+     * The FriendlyCaptcha secret
+     *
+     * @var string
+     */
+    protected $secret;
+
+    /**
+     * The FriendlyCaptch sitekey
+     *
+     * @var string
+     */
+    protected $sitekey;
+
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $http;
+
+    public function __construct($secret, $sitekey, $options = [])
+    {
+        $this->secret   = $secret;
+        $this->sitekey  = $sitekey;
+        $this->http     = new Client($options);
+    }
+}
