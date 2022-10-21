6cb88894e487be0163b3a5e938bdb88b4e4e3eef
ossycodes <ossyfizy009@gmail.com>
2022-10-21 16:07:34 +0100

    wip

--- src/FriendlyCaptcha.php
+++ src/FriendlyCaptcha.php
@@ -48,4 +48,50 @@ class FriendlyCaptcha
                 <script nomodule src="https://cdn.jsdelivr.net/npm/friendly-challenge@0.9.8/widget.min.js" async defer></script>
             EOF;
     }
+
+    public function renderWidget($attributes = [])
+    {
+        $attributes = $this->prepareAttributes($attributes);
+        return '<div' . $this->buildAttributes($attributes) . '></div>';
+    }
+
+    /**
+     * Prepare HTML attributes and ensure that the correct classes and attributes for captcha are inserted.
+     *
+     * @param array $attributes
+     *
+     * @return array
+     */
+    protected function prepareAttributes(array $attributes)
+    {
+        $attributes['data-sitekey'] = $this->sitekey;
+
+        if (isset($attributes['dark-theme'])) {
+            $attributes['class'] = 'frc-captcha dark';
+            unset($attributes['dark-theme']);
+            return $attributes;
+        }
+
+        $attributes['class'] = trim('frc-captcha');
+
+        return $attributes;
+    }
+
+    /**
+     * Build HTML attributes.
+     *
+     * @param array $attributes
+     *
+     * @return string
+     */
+    protected function buildAttributes(array $attributes)
+    {
+        $html = [];
+
+        foreach ($attributes as $key => $value) {
+            $html[] = $key . '="' . $value . '"';
+        }
+
+        return count($html) ? ' ' . implode(' ', $html) : '';
+    }
 }
