a8174d606354e691f06057ecb20de6baa17e130f
ossycodes <ossyfizy009@gmail.com>
2022-10-21 21:33:18 +0100

    add tests 🔥🚀

---
+++ tests/FriendlyCaptchaTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Ossycodes\FriendlyCaptcha\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Ossycodes\FriendlyCaptcha\FriendlyCaptcha;
+use Ossycodes\FriendlyCaptcha\FriendlyCaptchaServiceProvider;
+
+class FriendlyCaptchaTest extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return [FriendlyCaptchaServiceProvider::class];
+    }
+
+    /**
+     * @var FriendlyCaptchaTest
+     */
+    private $captcha;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->captcha = new FriendlyCaptcha('{secret-key}', '{site-key}');
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_render_unpkg_widget_script_correctly()
+    {
+        $this->assertTrue($this->captcha instanceof FriendlyCaptcha);
+
+        $expectedScriptOne  = '<script type="module" src="https://unpkg.com/friendly-challenge@0.9.8/widget.module.min.js" async defer></script>';
+        $expectedScriptTwo  = '<script nomodule src="https://unpkg.com/friendly-challenge@0.9.8/widget.min.js" async defer></script>';
+
+        $this->assertStringContainsString($expectedScriptOne, $this->captcha->renderWidgetScripts());
+        $this->assertStringContainsString($expectedScriptTwo, $this->captcha->renderWidgetScripts());
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_render_jsdelivr_widget_script_correctly()
+    {
+        $this->assertTrue($this->captcha instanceof FriendlyCaptcha);
+
+        $expectedScriptOne = '<script type="module" src="https://cdn.jsdelivr.net/npm/friendly-challenge@0.9.8/widget.module.min.js" async defer></script>';
+        $expectedScriptTwo = '<script nomodule src="https://cdn.jsdelivr.net/npm/friendly-challenge@0.9.8/widget.min.js" async defer></script>';
+
+        $this->assertStringContainsString($expectedScriptOne, $this->captcha->renderWidgetScripts('jsdelivr'));
+        $this->assertStringContainsString($expectedScriptTwo, $this->captcha->renderWidgetScripts('jsdelivr'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_render_widget_correctly()
+    {
+        $this->assertTrue($this->captcha instanceof FriendlyCaptcha);
+
+        $expectedWidget = '<div data-sitekey="{site-key}" class="frc-captcha"></div>';
+        $expectedWidgetWithCustomAttributes = '<div data-sitekey="{site-key}" class="frc-captcha dark"></div>';
+
+        $this->assertEquals($expectedWidget, $this->captcha->renderWidget());
+        $this->assertEquals($expectedWidgetWithCustomAttributes, $this->captcha->renderWidget(['dark-theme' => true]));
+    }
+}
