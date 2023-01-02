c5fe876690926550c4f1fb61d7d84d72e4a64e5b
Julian Dorn <post@wi-wissen.de>
2023-01-02 20:21:01 +0100

    use default friendlycaptcha.com server

--- src/config/friendlycaptcha.php
+++ src/config/friendlycaptcha.php
@@ -3,7 +3,7 @@
 return [
     'secret'            => env('FRIENDLY_CAPTCHA_SECRET'),
     'sitekey'           => env('FRIENDLY_CAPTCHA_SITEKEY'),
-    'puzzle_endpoint'   => env('FRIENDLY_CAPTCHA_PUZZLE_ENDPOINT', 'https://api.friendlycaptcha.eu/api/v1/puzzle'),
+    'puzzle_endpoint'   => env('FRIENDLY_CAPTCHA_PUZZLE_ENDPOINT', 'https://api.friendlycaptcha.com/api/v1/puzzle'),
     'verify_endpoint'   => env('FRIENDLY_CAPTCHA_VERIFY_ENDPOINT', 'https://api.friendlycaptcha.com/api/v1/siteverify'),
     'options'           => [
         'timeout'       => 30,
